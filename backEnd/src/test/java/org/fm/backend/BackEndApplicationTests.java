6084d3a388aafcdc2549c9f4e159b36752a4258d
Kita <10726427+moonskyelf@user.noreply.gitee.com>
2025-04-20 22:38:23 +0800

    springboot init

---
+++ backEnd/src/test/java/org/fm/backend/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fm.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
