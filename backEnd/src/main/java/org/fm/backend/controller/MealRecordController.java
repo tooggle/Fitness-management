9fe9d50b31635c5487f08de855a3f80f3439a801
YHISH-yhish <1308674513@qq.com>
2025-05-05 02:18:13 +0800

    modified:   backEnd/src/main/java/org/fm/backend/controller/MealRecordController.java 	deleted:    backEnd/src/main/java/org/fm/backend/util/AiTestHarness.java

--- backEnd/src/main/java/org/fm/backend/controller/MealRecordController.java
+++ backEnd/src/main/java/org/fm/backend/controller/MealRecordController.java
@@ -126,5 +126,9 @@ public class MealRecordController {
             this.success = success;
             this.suggestion = suggestion;
         }
+
+        public static AIResBuilder builder() {
+            return new AIResBuilder();
+        }
     }
 }
